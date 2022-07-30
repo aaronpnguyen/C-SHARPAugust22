24a7a647dce9e7c4e59056413b396004d1d036ff
Amiculay <aaronnguyen1123@gmail.com>
2022-07-29 23:10:25 -0400

    Initial commit

---
+++ Day1/Fundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+
+static void count() {
+    int i = 1;
+    while (i <= 255) {
+        Console.WriteLine(i);
+        i++;
+    }
+}
+
+static void fizzBuzz() {
+    int i = 1;
+    while (i <= 100) {
+        if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+        else if (i % 3 == 0) Console.WriteLine("Fizz");
+        else if (i % 5 == 0) Console.WriteLine("Buzz");
+        i++;
+    }
+}
+
+count();
+fizzBuzz();
