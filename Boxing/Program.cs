a643380bed6c0512ac342a008d2249325f60e3bf
Amiculay <aaronnguyen1123@gmail.com>
2022-07-30 04:01:00 -0400

    Initial commit

---
+++ Boxing/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Boxing {
+    class Program {
+        static void Main(string[] args) {
+            List<object> list = new List<object>();
+            list.Add(7);
+            list.Add(28);
+            list.Add(true);
+            list.Add(-1);
+            list.Add("chair");
+
+        int sum = 0;
+        foreach (var item in list) {
+            Console.WriteLine(item);
+            if (item is int) {
+                int value = (int)item;
+                sum += value;
+            }
+        }
+        Console.WriteLine($"Total sum from object: {sum}");
+        }
+    }
+}
