0e518f97dbacffb88ee092484955216b7d7a20bc
Amiculay <aaronnguyen1123@gmail.com>
2022-08-03 15:17:59 -0400

    Initial commit

---
+++ Day3/Portoflio/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController: Controller
+{
+    [HttpGet("/")]
+    public string Index()
+    {
+        return "This is my Index!";
+    }
+    [HttpGet("/projects")]
+    public string Projects()
+    {
+        return "This is my projects!";
+    }
+    [HttpGet("/contacts")]
+    public string Contacts()
+    {
+        return "This is my contacts!";
+    }
+}
