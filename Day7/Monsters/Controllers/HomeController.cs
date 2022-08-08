c02256e0c02d820977077f02052cfce9fa409b84
Amiculay <aaronnguyen1123@gmail.com>
2022-08-07 22:09:01 -0400

    Initial commit

---
+++ Day7/Monsters/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿// Using statements
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Monsters.Models;
+namespace Monsters.Controllers;
+    
+public class HomeController: Controller
+{
+    private MyContext _context;
+     
+    // here we can "inject" our context service into the constructor
+    public HomeController(MyContext context)
+    {
+        _context = context;
+    }
+     
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        // Get all Monsters
+        ViewBag.AllUsers = _context.Monsters.ToList();
+    
+         // Get Monsters with the LastName "Jefferson"
+         ViewBag.Jeffersons = _context.Monsters
+            .Where(u => u.Name == "Aaron");
+ 
+    	// Get the 5 most recently added Monsters
+        ViewBag.MostRecent = _context.Monsters
+    	    .OrderByDescending(u => u.CreatedAt)
+    	    .Take(5)
+    	    .ToList();
+ 
+	    return View();
+    }
+}  
